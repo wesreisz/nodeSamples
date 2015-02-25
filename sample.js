454f78403a1760cc2a21afcb2f36c51d806f5f35
Wesley Reisz <wes@wesleyreisz.com>
2015-02-25 10:42:13 -0500

    step4 simple webserver

--- sample.js
+++ sample.js
@@ -1,6 +1,18 @@
-//intro
+//Step 4
+// Load the http module to create an http server.
+var http = require('http');
 
-setInterval(function(){
-   console.log("world");
-},2000);
-console.log("hello ");
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello\n");
+  setInterval(function(){
+  	response.end("World\n");
+  },2000)
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
