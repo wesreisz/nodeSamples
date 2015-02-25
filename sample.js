39a5006fde55fb696cdea37b46bcd632867d0a06
Wesley Reisz <wes@wesleyreisz.com>
2015-02-25 11:41:30 -0500

    step6 simple tcp/ip chat server

--- sample.js
+++ sample.js
@@ -1,15 +1,27 @@
 //Step 5
 var net = require('net')
 var port = 8000;
+var sockets = [];
+ 
+var server = net.Server(function(socket){
+   console.log(socket.remoteAddress + ": connected");
+   sockets.push(socket);
 
-var server = net.createServer(function(socket){
-   socket.write("hello class\n");
-   socket.on('data',function(data) {
-	  console.log("rcvd (" + socket.remoteAddress + "): " + data)
-      socket.write("echo: " + data);
+   socket.on('data',function(data){
+	 console.log("rcvd (" + socket.remoteAddress + "): " + data);
+     for(var i=0; i<sockets.length; i++){
+         if(sockets[i] == socket){
+            continue;
+         }
+         sockets[i].write( socket.remoteAddress + ": " + data);
+     }
+   });
+   socket.on('end',function(){
+	 console.log("disconnected: " + socket.remoteAddress );
+     var i = sockets.indexOf(socket);
+     sockets.splice(i,1);
    });
 });
-
 server.listen(port);
 
 // Put a friendly message on the terminal
